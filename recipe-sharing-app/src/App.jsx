68d44a21a1dca8ecdea40417e2f74e654966672f
Ian Mwambuki <merlnadika@gmail.com>
2025-11-16 20:57:40 +0300

    Create App.jsx

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import RecipeList from './components/RecipeList'
+import AddRecipeForm from './components/AddRecipeForm'
+
+export default function App() {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Recipe Sharing App</h1>
+      <AddRecipeForm />
+      <hr />
+      <RecipeList />
+    </div>
+  )
+}
