cdd9e8038d1b9395eb4e1c5293d6b8c50db72d8d
Ian Mwambuki <merlnadika@gmail.com>
2025-12-30 14:46:13 +0300

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 import WelcomeMessage from './components/WelcomeMessage';
 import UserProfile from './components/UserProfile';
 
 function App() {
   return (
-    <div style={{ textAlign: "center", marginTop: "50px" }}>
+    <div style={{ textAlign: "center", marginTop: "20px" }}>
+      {/* Task 2 components */}
+      <Header />
+      <MainContent />
+      <Footer />
+
+      {/* Task 1 component */}
       <WelcomeMessage />
 
+      {/* Task 3 components with props */}
       <UserProfile 
         name="Alice" 
         age={25} 
