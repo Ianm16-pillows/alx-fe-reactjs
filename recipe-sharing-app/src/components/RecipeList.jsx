65db20b71982da5334047589f13382529c8e730e
Ian Mwambuki <merlnadika@gmail.com>
2025-12-31 14:02:11 +0300

    Update RecipeList.jsx

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,14 +2,19 @@ import { Link } from 'react-router-dom';
 import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
-  const recipes = useRecipeStore((state) => state.recipes);
+  const filteredRecipes = useRecipeStore(
+    (state) => state.filteredRecipes
+  );
 
   return (
     <div>
       <h2>Recipes</h2>
-      {recipes.length === 0 && <p>No recipes available.</p>}
 
-      {recipes.map((recipe) => (
+      {filteredRecipes.length === 0 && (
+        <p>No matching recipes found.</p>
+      )}
+
+      {filteredRecipes.map((recipe) => (
         <div key={recipe.id}>
           <h3>{recipe.title}</h3>
           <Link to={`/recipe/${recipe.id}`}>View Details</Link>
