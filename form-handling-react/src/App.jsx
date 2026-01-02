c965b35a71bd8e7db9da169237b0dfe33f05b19e
Ian Mwambuki <merlnadika@gmail.com>
2026-01-02 17:59:05 +0300

    Update App.jsx

--- form-handling-react/src/App.jsx
+++ form-handling-react/src/App.jsx
@@ -1,1 +1,15 @@
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+
+function App() {
+  return (
+    <div>
+      <RegistrationForm />
+      <hr />
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
 
