e2c6b1ef74d44d1a5d34b96efa501d765d319eb9
Ian Mwambuki <merlnadika@gmail.com>
2026-01-04 02:31:34 +0300

    Create App.jsx

---
+++ react-router-advanced/src/App.jsx
@@ -0,0 +1,38 @@
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import Post from "./components/Post";
+import ProtectedRoute from "./components/ProtectedRoute";
+
+const isAuthenticated = false; // simulate login
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuth={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        <Route path="/post/:id" element={<Post />} />
+
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
