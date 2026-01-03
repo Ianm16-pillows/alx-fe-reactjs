f1b8ba7e364bce22d834adc3c70b5e4eaa0e22e4
Ian Mwambuki <merlnadika@gmail.com>
2026-01-04 02:48:38 +0300

    Update App.jsx

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -1,33 +1,49 @@
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "react-query";
+
+import RegistrationForm from "./components/RegistrationForm";
+import FormikForm from "./components/FormikForm";
+import PostsComponent from "./components/PostsComponent";
 import Home from "./components/Home";
 import Profile from "./components/Profile";
+import BlogPost from "./components/BlogPost";
 import ProtectedRoute from "./components/ProtectedRoute";
-import BlogPost from "./components/BlogPost"; // NEW
 
-const isAuthenticated = false; // simulate login
+const queryClient = new QueryClient();
+const isAuthenticated = false; // simulated auth
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
+    <QueryClientProvider client={queryClient}>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
 
-        <Route
-          path="/profile/*"
-          element={
-            <ProtectedRoute isAuth={isAuthenticated}>
-              <Profile />
-            </ProtectedRoute>
-          }
-        />
+          {/* Form routes */}
+          <Route path="/register" element={<RegistrationForm />} />
+          <Route path="/formik-register" element={<FormikForm />} />
 
-        {/* Dynamic route for blog post */}
-        <Route path="/blog/:id" element={<BlogPost />} />
+          {/* React Query */}
+          <Route path="/posts" element={<PostsComponent />} />
 
-        {/* Catch-all */}
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
-    </Router>
+          {/* Nested & Protected Profile routes */}
+          <Route
+            path="/profile/*"
+            element={
+              <ProtectedRoute>
+                <Profile />
+              </ProtectedRoute>
+            }
+          />
+
+          {/* Dynamic blog route */}
+          <Route path="/blog/:id" element={<BlogPost />} />
+
+          {/* Catch-all */}
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
+      </Router>
+    </QueryClientProvider>
   );
 }
 
