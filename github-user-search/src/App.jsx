fd23adb043f97354dfa064a61604d4aae8479a56
Ian Mwambuki <merlnadika@gmail.com>
2026-01-03 17:51:56 +0300

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -3,12 +3,9 @@ import Search from "./components/Search";
 export default function App() {
   return (
     <div className="min-h-screen bg-gray-50 flex flex-col items-center pt-10 px-4">
-      {/* App Header */}
       <h1 className="text-3xl font-bold mb-6 text-center">
         GitHub User Search
       </h1>
-
-      {/* Search Component */}
       <Search />
     </div>
   );
