7fd1f848b1b7ad8ddf422c107185452de19e339f
Ian Mwambuki <merlnadika@gmail.com>
2026-01-04 02:42:59 +0300

    Create useAuth.js

---
+++ react-router-advanced/src/hooks/useAuth.js
@@ -0,0 +1,7 @@
+// Simple hook to simulate authentication
+const useAuth = () => {
+  const isAuthenticated = false; // replace with real login logic if needed
+  return { isAuthenticated };
+};
+
+export default useAuth;
