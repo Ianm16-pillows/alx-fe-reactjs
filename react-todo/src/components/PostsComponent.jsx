0026f525dde02f0d3f74dfe023d7f2236c4b95c2
Ian Mwambuki <merlnadika@gmail.com>
2026-01-04 02:57:18 +0300

    Create PostsComponent.jsx

---
+++ react-todo/src/components/PostsComponent.jsx
@@ -0,0 +1,35 @@
+import { useQuery } from "react-query";
+
+const fetchPosts = async () => {
+  const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+  if (!res.ok) throw new Error("Network error");
+  return res.json();
+};
+
+const PostsComponent = () => {
+  const { data: posts, isLoading, isError, refetch } = useQuery("posts", fetchPosts, {
+    cacheTime: 1000 * 60 * 5,
+    staleTime: 1000 * 60 * 1,
+    refetchOnWindowFocus: false,
+    keepPreviousData: true,
+  });
+
+  if (isLoading) return <p>Loading posts...</p>;
+  if (isError) return <p>Error fetching posts</p>;
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      <button onClick={() => refetch()}>Refetch Posts</button>
+      <ul>
+        {posts.slice(0, 10).map((post) => (
+          <li key={post.id}>
+            <strong>{post.title}</strong>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default PostsComponent;
