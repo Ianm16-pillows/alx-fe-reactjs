92168a57142501ce0aa5fb023770b7872536e147
Ian Mwambuki <merlnadika@gmail.com>
2026-01-03 15:59:18 +0300

    Create App.jsx

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="p-4">
+      <h1 className="text-xl font-bold">
+        Recipe Sharing Platform
+      </h1>
+    </div>
+  );
+}
+
+export default App;
