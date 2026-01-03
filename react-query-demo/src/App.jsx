fca6a3a621b77f70459e67f06b68f17b96a33c5f
Ian Mwambuki <merlnadika@gmail.com>
2026-01-04 02:24:25 +0300

    Update App.jsx

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,14 +1,39 @@
-import { QueryClient, QueryClientProvider } from "react-query";
-import PostsComponent from "./components/PostsComponent";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import Home from "./components/Home";
+import Profile from "./components/Profile";
+import ProfileDetails from "./components/ProfileDetails";
+import ProfileSettings from "./components/ProfileSettings";
+import Post from "./components/Post";
+import ProtectedRoute from "./components/ProtectedRoute";
 
-const queryClient = new QueryClient();
+const isAuthenticated = false; // simulate login
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <PostsComponent />
-    </QueryClientProvider>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route
+          path="/profile/*"
+          element={
+            <ProtectedRoute isAuth={isAuthenticated}>
+              <Profile />
+            </ProtectedRoute>
+          }
+        >
+          <Route path="details" element={<ProfileDetails />} />
+          <Route path="settings" element={<ProfileSettings />} />
+        </Route>
+
+        <Route path="/post/:id" element={<Post />} />
+
+        {/* Catch-all route */}
+        <Route path="*" element={<Navigate to="/" />} />
+      </Routes>
+    </Router>
   );
 }
 
 export default App;
+
