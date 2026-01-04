d756ffe136495ca1efe9d479a7c8bf8cc7a97743
Ian Mwambuki <merlnadika@gmail.com>
2026-01-04 03:03:03 +0300

    Create TodoList.test.jsx

---
+++ react-todo/src/__tests__/TodoList.test.jsx
@@ -0,0 +1,34 @@
+import { render, screen, fireEvent } from "@testing-library/react";
+import TodoList from "../components/TodoList";
+
+describe("TodoList Component", () => {
+  test("renders initial todos", () => {
+    render(<TodoList />);
+    expect(screen.getByText("Learn React")).toBeInTheDocument();
+    expect(screen.getByText("Build Todo App")).toBeInTheDocument();
+  });
+
+  test("can add a new todo", () => {
+    render(<TodoList />);
+    const input = screen.getByPlaceholderText("Add todo...");
+    const addButton = screen.getByText("Add");
+    fireEvent.change(input, { target: { value: "New Todo" } });
+    fireEvent.click(addButton);
+    expect(screen.getByText("New Todo")).toBeInTheDocument();
+  });
+
+  test("can toggle a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    fireEvent.click(todo);
+    expect(todo).toHaveStyle("text-decoration: line-through");
+  });
+
+  test("can delete a todo", () => {
+    render(<TodoList />);
+    const todo = screen.getByText("Learn React");
+    const deleteButton = todo.querySelector("button");
+    fireEvent.click(deleteButton);
+    expect(todo).not.toBeInTheDocument();
+  });
+});
