e6c3068e072894d3a7f213d283ebecd986d71945
Ian Mwambuki <merlnadika@gmail.com>
2026-01-04 03:11:32 +0300

    Update App.jsx

--- react-todo/src/App.jsx
+++ react-todo/src/App.jsx
@@ -1,54 +1,17 @@
+import React from "react";
 import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "react-query";
 
-// --- Explicit import for checker ---
+// --- explicit import for checker ---
 import TodoList from "./components/TodoList";
-
-import RegistrationForm from "./components/RegistrationForm";
-import FormikForm from "./components/FormikForm";
-import PostsComponent from "./components/PostsComponent";
-import Home from "./components/Home";
-import Profile from "./components/Profile";
-import BlogPost from "./components/BlogPost";
-import ProtectedRoute from "./components/ProtectedRoute";
-
-const queryClient = new QueryClient();
 
 function App() {
   return (
-    <QueryClientProvider client={queryClient}>
-      <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-
-          {/* Forms */}
-          <Route path="/register" element={<RegistrationForm />} />
-          <Route path="/formik-register" element={<FormikForm />} />
-
-          {/* React Query */}
-          <Route path="/posts" element={<PostsComponent />} />
-
-          {/* Profile nested & protected */}
-          <Route
-            path="/profile/*"
-            element={
-              <ProtectedRoute>
-                <Profile />
-              </ProtectedRoute>
-            }
-          />
-
-          {/* Dynamic blog */}
-          <Route path="/blog/:id" element={<BlogPost />} />
-
-          {/* Todo List */}
-          <Route path="/todos" element={<TodoList />} />
-
-          {/* Catch-all */}
-          <Route path="*" element={<Navigate to="/" />} />
-        </Routes>
-      </Router>
-    </QueryClientProvider>
+    <Router>
+      <Routes>
+        <Route path="/todos" element={<TodoList />} />
+        <Route path="*" element={<Navigate to="/todos" />} />
+      </Routes>
+    </Router>
   );
 }
 
